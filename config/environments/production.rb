1f33dac876dab260e273abb3d6a606f003964aec
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-06-22 22:57:00 +0530

    host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = {:host => "128.199.112.135"}
   ActionMailer::Base.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
