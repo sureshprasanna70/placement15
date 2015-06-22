9a9acda865bf85d95d647b418247fd72bdc0037d
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-06-22 15:24:30 +0530

    Notification ticker

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 $('#faq').accordion();
+$('.ticker1, .ticker2').easyTicker({
+    direction: 'up',
+    easing: 'swing',
+    speed: 'slow',
+    interval: 2000,
+    height: 'auto',
+    visible: 0,
+    mousePause: 1,
+    controls: {
+          up: '',
+      down: '',
+      toggle: '',
+      playText: 'Play',
+      stopText: 'Stop'
+    }
+});
 });
