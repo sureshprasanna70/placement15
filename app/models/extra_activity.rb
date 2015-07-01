7f2a882de4c4499aa8ffaf263b1e9876f7eb7ae0
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-07-01 23:34:13 +0530

    validation for all models

--- app/models/extra_activity.rb
+++ app/models/extra_activity.rb
@@ -1,3 +1,10 @@
 class ExtraActivity < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :area_of_interest
+  validates_presence_of :extra_curricular
+  validates_presence_of :hobby
+  validates_presence_of :responsibilty
+  validates_presence_of :electives
+  validates_presence_of :current
+  validates_presence_of :history
 end
