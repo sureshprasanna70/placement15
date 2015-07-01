ef3e7ac029ceab4d3a0b0db062a244d90d21394f
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-07-01 21:20:27 +0530

    protecting admin and workstatus

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  before_filter :sign_in_check
   def index
   end
 
@@ -10,5 +11,15 @@ class AdminController < ApplicationController
     puts params[:start_number]
     puts params[:end_number]
     RollWorker.perform_async(params[:start_number],params[:end_number])
+  end
+  private
+  def sign_in_check
+    if not user_signed_in?
+      if not current_user.has_role?"admin"
+        redirect_to root_path
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
