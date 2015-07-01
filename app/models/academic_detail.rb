429747a8633686d33a4fbff12b97a51eb5440ccf
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-07-01 22:44:15 +0530

    validations in semester marks and academic detail

--- app/models/academic_detail.rb
+++ app/models/academic_detail.rb
@@ -1,3 +1,10 @@
 class AcademicDetail < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :course
+  validates_presence_of :institution
+  validates_presence_of :examination_authority
+  validates_presence_of :pass_year
+  validates_numericality_of :pass_year
+  validates_presence_of :percentage
+  validates_numericality_of :percentage
 end
--- app/models/semester_mark.rb
+++ app/models/semester_mark.rb
@@ -1,3 +1,9 @@
 class SemesterMark < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :semester
+  validates_numericality_of :semester
+  validates_presence_of :gpa
+  validates_numericality_of :gpa
+  validates_presence_of :pass_year
+  validates_numericality_of :pass_year
 end
