e8442aa9f54bbc439be2ed1609c29714efc754d2
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-07-05 00:18:59 +0530

    db conf and excel worker optimize

--- app/workers/excel_worker.rb
+++ app/workers/excel_worker.rb
@@ -1,27 +1,22 @@
 class ExcelWorker
   include Sidekiq::Worker
+  sidekiq_options :queue => :default, :retry => false, :backtrace => true
   def perform(path)
     spreadsheet=Roo::Excel.new(path)
     sheet=spreadsheet.sheet(1)
-    
-    rollno_pass={}
     start_number=sheet.row(2)[0].to_i.to_s
     end_number=sheet.row(sheet.last_row)[0].to_i.to_s
-      (2..sheet.last_row).each do |row_num|
-         i=sheet.row(row_num)[0].to_i.to_s
-         password=Random.rand(20**20)
-         puts i 
-         puts password
-         User.create!({:email => "", :phone=>"",:registerno=>i, :password => password, :password_confirmation => password })
-         rollno_pass[i]=password
-      end
     directory=File.dirname(Rails.root)
     directory=directory+"/placement/data/excelsheet"
     file=start_number+"-"+end_number+".csv"
     CSV.open(File.join(directory,file), 'w' ) do |writer|
-      rollno_pass.each do |key,value|
-        puts value
-        writer<<[key,value]
+      (2..sheet.last_row).each do |row_num|
+         i=sheet.row(row_num)[0].to_i.to_s
+         name=sheet.row(row_num)[1].to_s 
+         password=Random.rand(15**15)
+         puts "REGISTER NUMBER #{i} NAME #{name} PASSWORD #{password} "
+         User.create!({:email => "", :phone=>"",:registerno=>i, :password => password, :password_confirmation => password,:name=>name })
+         writer<<[i,password]
       end
     end
     ws=Workstatus.new
@@ -29,25 +24,22 @@ class ExcelWorker
     ws.status="done"
     ws.filename="excelsheet/"+file
     ws.save
+    puts "UG ENDS"
+    puts "PG STARTS"
     pgsheet=spreadsheet.sheet(3)
-    rollno_pass={}
-    start_number=sheet.row(2)[0].to_i.to_s
-    end_number=sheet.row(sheet.last_row)[0].to_i.to_s
-      (2..sheet.last_row).each do |row_num|
-         i=sheet.row(row_num)[0].to_i.to_s
-         password=Random.rand(20**20)
-         puts i 
-         puts password
-         User.create!({:email => "", :phone=>"",:registerno=>i, :password => password, :password_confirmation => password })
-         rollno_pass[i]=password
-      end
     directory=File.dirname(Rails.root)
     directory=directory+"/placement/data/excelsheet"
     file=start_number+"-"+end_number+".csv"
+    start_number=sheet.row(2)[0].to_i.to_s
+    end_number=sheet.row(sheet.last_row)[0].to_i.to_s
     CSV.open(File.join(directory,file), 'w' ) do |writer|
-      rollno_pass.each do |key,value|
-        puts value
-        writer<<[key,value]
+      (2..sheet.last_row).each do |row_num|
+         i=sheet.row(row_num)[0].to_i.to_s
+         password=Random.rand(15**15)
+         name=sheet.row(row_num)[1].to_s 
+         puts "REGISTER NUMBER #{i} NAME #{name} PASSWORD #{password} "
+         User.create!({:email => "", :phone=>"",:registerno=>i, :password => password, :password_confirmation => password,:name=>name })
+         writer<<[i,password]
       end
     end
     ws=Workstatus.new
@@ -55,6 +47,5 @@ class ExcelWorker
     ws.status="done"
     ws.filename="excelsheet/"+file
     ws.save
-
   end
 end
