35a137a40d27f4122ebec7548b495661730cb696
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-07-06 20:55:14 +0530

    ajaxed

--- app/assets/javascripts/college_profiles.js
+++ app/assets/javascripts/college_profiles.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $("#college_profile_degree").change(function(){
+    url="/college_profiles/"+this.value
+    console.log(url);
+  })
+})
