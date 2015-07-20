51bf66fdf4a7c0280d72e783f29824abd5694b10
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-07-20 23:07:26 +0530

    cron job

---
+++ config/initializers/disable_scheduler.rb
@@ -0,0 +1,10 @@
+scheduler = Rufus::Scheduler.new
+scheduler.cron '5 0 * * *' do
+  users=User.where(:can_edit=>true)
+  users.each do |user|
+    user.can_edit=false
+    user.save!
+    puts "#{user.registerno}'s edit capability disabled"
+  end
+ 
+end
