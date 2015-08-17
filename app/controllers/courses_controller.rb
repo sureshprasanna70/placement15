5f05e30ca1c83f6a14634a65a79374193a70466d
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-08-17 13:09:09 +0530

    role check courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -74,6 +74,10 @@ class CoursesController < ApplicationController
   def sign_in_check
     if not user_signed_in?
       redirect_to new_user_session_path
+    else
+      if not current_user.has_role?"admin"
+        redirect_to root_path
+      end
     end
   end
 end
