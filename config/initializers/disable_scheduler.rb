bc3d9a1e4e6879d6841e0cfa0d7bd914107b5ec7
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-08-17 13:53:14 +0530

    disable scheduler

--- config/initializers/disable_scheduler.rb
+++ config/initializers/disable_scheduler.rb
@@ -1,11 +1,11 @@
 scheduler = Rufus::Scheduler.new
 scheduler.cron '5 0 * * *' do
-  users=User.where(:can_edit=>true)
-  registerno=" "
-  users.each do |user|
-    user.can_edit=false
-    user.save!
-    registerno+=user.registerno+","
-  end
-  FeedbackMailer.scheduler_message(registerno).deliver
+  #users=User.where(:can_edit=>true)
+  #registerno=" "
+  #users.each do |user|
+    #user.can_edit=false
+    #user.save!
+    #registerno+=user.registerno+","
+  #end
+  #FeedbackMailer.scheduler_message(registerno).deliver
 end
