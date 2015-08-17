165fe0c1ef6cb4b7a2938fc843fb478cc57fd923
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-08-17 22:46:30 +0530

    working on cappy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,19 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
+set :application, 'Placements'
+set :repo_url, 'https://github.com/sureshprasanna70/placement15.git'
+set :branch ,"master"
+set :user,"cuic"
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/cuic/'
+set :rails_env, "production"
+set :scm, :git
 
-# Default value for :scm is :git
-# set :scm, :git
+set :format, :pretty
 
-# Default value for :format is :pretty
-# set :format, :pretty
+set :log_level, :debug
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
