1bddb6c87bcd1fc17bf9b0b5d75c8cc7c63ba4e0
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-08-19 12:41:13 +0530

    done with academic details

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
 gem 'mysql2'
+gem 'sass-rails'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- app/controllers/academic_details_controller.rb
+++ app/controllers/academic_details_controller.rb
@@ -42,6 +42,8 @@ class AcademicDetailsController < ApplicationController
   # PATCH/PUT /academic_details/1.json
   def update
     respond_to do |format|
+      puts academic_detail_params
+      puts params[:academic_detail][:pg_authority]
       if @academic_detail.update(academic_detail_params)
         format.html { redirect_to resume_path, notice: 'Academic detail was successfully updated.' }
         format.json { render :show, status: :ok, location: @academic_detail }
@@ -74,7 +76,7 @@ class AcademicDetailsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def academic_detail_params
-    params.require(:academic_detail).permit(:sslc_mark, :sslc_institute, :sslc_authority,:sslc_year, :hsc_percentage,:hsc_mark, :hsc_institute, :hsc_authority,:hsc_year,:hsc_percentage,:ug_mark, :ug_institute, :ug_authority,:ug_year, :pg_percentage,:pg_mark, :pg_institute, :pg_authority,:pg_year, :pg_percentage,:user_id)
+    params.require(:academic_detail).permit(:sslc_mark, :sslc_institute, :sslc_authority,:sslc_year,:hsc_mark, :hsc_institute, :hsc_authority,:hsc_year,:ug_mark, :ug_institute, :ug_authority,:ug_year, :pg_mark, :pg_institute, :pg_authority,:pg_year,:user_id)
   end
   def sign_in_check
     if not user_signed_in?
